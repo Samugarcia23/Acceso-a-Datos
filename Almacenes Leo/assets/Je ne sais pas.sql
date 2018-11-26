d195bd65cf510cfd1b40b12cd9682fbf6a943a83
Sam <samugarcia23@gmail.com>
2018-11-26 09:17:16 +0100

    mAH cOsah

--- Almacenes Leo/assets/Je ne sais pas.sql
+++ Almacenes Leo/assets/Je ne sais pas.sql
@@ -6,14 +6,25 @@ INNER JOIN Envios E ON A.ID = E.AlmacenPreferido
 SELECT *
 	FROM Almacenes AS A
 		INNER JOIN Distancias AS D
-			ON A.ID = D.IDAlmacen1 AND D.IDAlmacen2 = 20
+			ON A.ID = D.IDAlmacen1 AND D.IDAlmacen2 = A.ID
 	WHERE A.ID < 20
 UNION
 SELECT *
 	FROM Almacenes AS A
 		INNER JOIN Distancias AS D
-			ON D.IDAlmacen1 = 20 AND A.ID = D.IDAlmacen2
+			ON D.IDAlmacen1 = A.ID AND A.ID = D.IDAlmacen2
 	WHERE A.ID > 20
+
+	SELECT IDAlmacen1 AS ID1, IDAlmacen2 AS ID2, Distancia
+	FROM Distancias 
+	UNION
+	SELECT IDAlmacen2 AS ID1, IDAlmacen1 AS ID2, Distancia
+	FROM Distancias 
+
+
+
+
+
 
 	SELECT * FROM Almacenes AS A INNER JOIN Distancias AS D ON A.ID = D.IDAlmacen1 AND IDAlmacen2 = 20 WHERE A.ID < 20 UNION SELECT * FROM Almacenes AS A INNER JOIN Distancias AS D ON D.IDAlmacen1 = 20 AND A.ID = D.IDAlmacen2 WHERE A.ID > 20 ORDER BY ID
 
